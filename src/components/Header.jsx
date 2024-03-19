8d7e3095906c1e8a6034fee83a6fb783add5d6b1
shantanu buddekar <buddekar.shantanu@gmail.com>
2024-03-19 18:26:10 +0530

    completed pages login and sign ups

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ export default function Header(props) {
     const navigate = useNavigate()
   return(
 
-    <Container>
+    <Container className="flex a-center j-between">
         <div className="logo">
             <img src={logo} alt="Logo" />
         </div>
@@ -18,4 +18,20 @@ export default function Header(props) {
   ) 
 }
 
-const Container = styled.div``;
+const Container = styled.div`
+padding: 0 4rem;
+.logo{
+  img{
+    height: 5rem;
+  }
+}
+button{
+  padding: 0.5rem 1rem;
+  background-color:#e50914;
+  border: none;
+  cursor: white;
+  border-radius: 0.2rem;
+  font-weight: bolder;
+  fontsize: 1.05rem 
+}
+`;
